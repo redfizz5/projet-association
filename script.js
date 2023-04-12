830a6e12432210190182b87e2282dd84576e348c
redfizz5 <antoinedsr50@gmail.com>
2023-04-12 21:05:55 +0200

    premier envoi

---
+++ script.js
@@ -0,0 +1,8 @@
+/*PARALLAX*/
+
+let parallax = document.querySelector('.parallax');
+
+window.onscroll = () => {
+    let scrollTop = document.documentElement.scrollTop;
+    parallax.style.backgroundPositionY = scrollTop * 0.7 + 'px';
+}
